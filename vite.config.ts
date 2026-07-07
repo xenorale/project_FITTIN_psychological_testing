e9ad2cd1aff08018ca47007dc578031342d31036
Dmitry Batanov <dima.batanov2019@mail.ru>
2026-07-07 15:55:20 +0300

    добавил dev-прокси vite для api

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    open: true
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
   }
 })
