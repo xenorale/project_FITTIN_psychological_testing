292632d132f2e0f76fb1b47ceaeb0a60314be434
Dmitry Batanov <dima.batanov2019@mail.ru>
2026-07-09 14:08:03 +0300

    добавил axios client и vite-env типы

---
+++ src/api/client.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  timeout: 10000
+})
+
+api.interceptors.request.use(config => {
+  const isPublicInvite = /^\/api\/invites\/[^/]+/.test(config.url || '')
+  if (!isPublicInvite) {
+    const token = localStorage.getItem('hr_token')
+    if (token) {
+      config.headers.Authorization = 'Bearer ' + token
+    }
+  }
+  return config
+})
